20822f15c36eca97ed00d6bef8d51785ed2ef299
Hendrik Samkay <erick.pjb@gmail.com>
2026-02-04 11:20:59 +0900

    Helpers

--- app/Helpers/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!function_exists('public_path')) {
-    function public_path($path = '')
-    {
-        return app()->basePath('public') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
-    }
-}
